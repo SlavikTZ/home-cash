f57ffc6066ec8e32bf2aac8405c7f992707f915e
Vyacheslav Lysenko <vyacheslavlysen@gmail.com>
2017-12-28 14:25:06 +0200

    Init aplication

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const configDB = require('./config/database');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect(configDB.uri, {useMongoClient: true}, (err) => {
+    if(err) {
+        console.log("Cloud NOT connect to database")
+    } else {
+        console.log(`Connected to database: ${configDB.db}`);
+}
+});
+
+app.use(express.static(`${__dirname}/../dist/`));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server runing on port:${port}`);
+});
