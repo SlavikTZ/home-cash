1c2a11a7f55df52cdbb1d119b9d737d39c77cf04
Vyacheslav Lysenko <vyacheslavlysen@gmail.com>
2017-12-29 11:40:59 +0200

    6. Валидация формы login"

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {AuthComponent} from './auth.component';
-import {AuthRoutingModule} from "./auth-routing.module";
+import {AuthRoutingModule} from './auth-routing.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        AuthRoutingModule
+        AuthRoutingModule,
+        SharedModule
 
     ],
     declarations: [LoginComponent, RegistrationComponent, AuthComponent]
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'cash-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'cash-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
+    password = 'Пароль';
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            'email': new FormControl('', [Validators.required, Validators.email]),
+            'password': new FormControl('', [Validators.required, Validators.minLength(6)]),
+        });
+    }
+
+    sendForm() {
+        console.log(this.form);
+    }
 
 }
