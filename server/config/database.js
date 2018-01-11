79369d25d005a9bf3cb8823dfc03cffacbb7dc10
Dmitriy <shal91@mail.ru>
2018-01-11 14:00:16 +0200

    git ignore in server/ folder

--- server/config/database.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto').randomBytes(256).toString('hex');
-const db = 'tree';
-module.exports = {
-    uri: `mongodb://127.0.0.1:27017/${db}`,
-    secret: crypto,
-    db: `${db}`
-}
