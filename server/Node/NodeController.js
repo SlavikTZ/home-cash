e473f8300f0e450ece92ccdf976002f472d18c98
Vyacheslav Lysenko <vyacheslavlysen@gmail.com>
2018-01-19 20:22:52 +0200

    add async add await

--- server/Node/NodeController.js
+++ server/Node/NodeController.js
@@ -10,97 +10,100 @@ module.exports = {
     /**
      * NodeController.list()
      */
-    list: function (req, res) {
-        NodeModel.find(function (err, Nodes) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Node.',
-                    error: err
-                });
-            }
-            return res.json(Nodes);
-        });
+    list: async (req, res, next) => {
+        try {
+            const Nodes = await NodeModel.find();
+            res.json(Nodes);
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error when getting Node.',
+                error: err
+            });
+        }
+
     },
 
     /**
      * NodeController.show()
      */
-    show: function (req, res) {
-        var id = req.params.id;
-        if (id === 'root') {
-            id = null;
-        }
-        NodeModel.find({parent_id: id}, function (err, Nodes) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Node.',
-                    error: err
-                });
+    show: async (req, res, next) => {
+        try {
+
+            var id = req.params.id;
+
+            if (id === 'root') {
+                id = null;
             }
+
+            const Nodes = await NodeModel.find({parent_id: id});
+
             if (Nodes.length == 0) {
                 return res.status(404).json({
                     message: 'No such Node'
                 });
             }
+
             return res.status(200).json(Nodes);
-        });
+
+        } catch (err) {
+            return res.status(500).json({
+                message: 'Error when getting Node.',
+                error: err
+            });
+        }
     },
 
     /**
      * NodeController.create()
      */
-    create: function (req, res) {
-        var Node = new NodeModel({
-            parent_id: req.body.parent_id,
-            name: 'New Node',
-            isChildren: false
-        });
-        Node.save()
-            .then((node) => {
-                NodeModel.findOne({_id: req.body.parent_id})
-                    .update({$set: {isChildren: true}})
-                    .then((parentNode) => {
-                        return res.status(201).json(node);
-                    })
-                    .catch((err) => conle.log(err));
-
-            })
-            .catch((err) => {
-                return res.status(500).json({
-                    message: 'Error when creating Node',
-                    error: err
-                });
+    create: async (req, res, next) => {
+        try {
+            const Node = new NodeModel({
+                parent_id: req.body.parent_id,
+                name: 'New Node',
+                isChildren: false
             });
+
+
+            const parentNode = await NodeModel.findOne({_id: req.body.parent_id});
+            if (!parentNode) {
+                res.status(400).json();
+            }
+            let node = await Node.save();
+            await parentNode.update({$set: {isChildren: true}});
+            res.status(201).json(node);
+
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error when creating Node',
+                error: err
+            });
+        }
     },
 
     /**
      * NodeController.update()
      */
-    update: function (req, res) {
-        var id = req.params.id;
-        NodeModel.findOne({_id: id}, function (err, Node) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Node',
-                    error: err
-                });
-            }
-            if (!Node) {
+    update: async (req, res, next) => {
+        try {
+            const id = req.params.id;
+            let node = await NodeModel.findOne({_id: id});
+
+            if (!node) {
                 return res.status(404).json({
                     message: 'No such Node'
                 });
             }
-            Node.name = req.body.name ? req.body.name : Node.name;
-            Node.save(function (err, Node) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating Node.',
-                        error: err
-                    });
-                }
-                return res.json(Node);
+
+            node.name = req.body.name ? req.body.name : node.name;
+            res.json(await node.save());
+
+        } catch (err) {
+            res.status(500).json({
+                message: 'Error when updating Node.',
+                error: err
             });
-        });
+        }
     },
 
     /**
